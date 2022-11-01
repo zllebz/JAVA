3ab233a89ac0af777bb43ef80883c1154c282442
zllebz <dumkroeng-h@rmutp.ac.th>
2022-11-01 07:42:44 +0700

    README.md

--- lesson2/chapter3.java
+++ lesson2/chapter3.java
@@ -1,7 +1,24 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class chapter3 {
     public static void main(String[] args) {
-        System.out.println("test error");
+        Scanner sc = new Scanner(System.in);
+        String space = "";
+        int i ;
+        do{
+        System.out.print("Number : ");
+        i = sc.nextInt();
+        for (int num = 1; num <= i; num++) {
+            for (int h = 1; h < num; h++) {
+                System.out.print(" ");
+            }
+            for (int w = num; w <= i; w++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }    
+        }while(i >= 1);
     }
 }
